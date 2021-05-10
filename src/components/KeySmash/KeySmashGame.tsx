a111d98b76a0694e83d61ee8f3f2f526016d52f3
kawre <69250723+kawre@users.noreply.github.com>
2021-05-10 22:25:51 +0200

    Keycaps layout working 😍⌨️

--- src/components/KeySmash/KeySmashGame.tsx
+++ src/components/KeySmash/KeySmashGame.tsx
@@ -1,24 +1,23 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useAuth } from "../../contexts/AuthContext";
+import KeyCapButton from "../KeyCapButton";
 // Types -------------------------------------------------------------------------
 
-// Styled ------------------------------------------------------------------------
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
-const GameContainer = styled.div`
-  width: 700px;
-  height: 500px;
-`;
+interface KeySmash {
+  rows: {
+    numbersRow: [];
+    topRow: [];
+    middleRow: [];
+    bottomRow: [];
+  };
+}
 
 // Component ---------------------------------------------------------------------
 const KeySmashGame = () => {
   const { getLayout } = useAuth();
   const [currentLayout, setCurrentLayout] = useState<string>("qwerty");
+  const [rows, setRows] = useState<KeySmash["rows"] | undefined>(undefined);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -26,8 +25,13 @@ const KeySmashGame = () => {
       setLoading(true);
 
       try {
-        const siema = await getLayout(currentLayout);
-        console.log(siema);
+        const response: any = await getLayout(currentLayout);
+        setRows({
+          numbersRow: response.numbersRow,
+          topRow: response.topRow,
+          middleRow: response.middleRow,
+          bottomRow: response.bottomRow,
+        });
       } catch {
         console.log("error");
       }
@@ -38,11 +42,67 @@ const KeySmashGame = () => {
     fetchLayout();
   }, []);
 
+  if (loading) return null;
   return (
     <Wrapper>
-      <GameContainer></GameContainer>
+      <GameContainer>
+        <NumbersRow>
+          {rows?.numbersRow.map((key: string) => {
+            return <KeyCapButton>{key}</KeyCapButton>;
+          })}
+        </NumbersRow>
+        <TopRow>
+          {rows?.topRow.map((key: string) => {
+            return <KeyCapButton>{key}</KeyCapButton>;
+          })}
+        </TopRow>
+        <MiddleRow>
+          {rows?.middleRow.map((key: string) => {
+            return <KeyCapButton>{key}</KeyCapButton>;
+          })}
+        </MiddleRow>
+        <BottomRow>
+          {rows?.bottomRow.map((key: string) => {
+            return <KeyCapButton>{key}</KeyCapButton>;
+          })}
+        </BottomRow>
+      </GameContainer>
     </Wrapper>
   );
 };
 
 export default KeySmashGame;
+
+// Styled ------------------------------------------------------------------------
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const GameContainer = styled.div`
+  width: 100%;
+  height: 400px;
+`;
+
+// Rows
+
+const Row = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  margin-bottom: 10px;
+`;
+
+const NumbersRow = Row;
+
+const TopRow = Row;
+
+const MiddleRow = Row;
+
+const BottomRow = Row;
