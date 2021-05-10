c4e359e206b03bf5e093333caad20a87eaf9e581
kawre <69250723+kawre@users.noreply.github.com>
2021-05-10 21:20:05 +0200

    Init 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import styled from "styled-components";
+import KeySmashGame from "./components/KeySmash/KeySmashGame";
+import { AuthProvider } from "./contexts/AuthContext";
+
+const Wrapper = styled.div`
+  background: black;
+  width: 100vw;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div>
-      <h1>siema</h1>
-    </div>
+    <AuthProvider>
+      <Wrapper>
+        <KeySmashGame />
+      </Wrapper>
+    </AuthProvider>
   );
 }
 
