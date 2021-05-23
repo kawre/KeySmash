67816724cc26744ff30e1ab84eb631dec22bdb51
kawre <69250723+kawre@users.noreply.github.com>
2021-05-23 14:05:50 +0200

    Init
    Starting all over again because of babel

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,26 @@
-import React, { useState } from "react";
-import { Route, Switch } from "react-router-dom";
-import styled from "styled-components";
-import Header from "./components/Header";
-import KeySmashGame from "./components/KeySmash/KeySmashGame";
-import LoginForm from "./components/LoginForm";
-import { RegisterForm } from "./components/RegisterForm";
-import TypingGame from "./components/TypingGame/TypingGame";
-import { AuthProvider } from "./contexts/AuthContext";
-import { DataProvider } from "./contexts/DataContext";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const [show, setShow] = useState<boolean>(true);
+function App() {
   return (
-    <AuthProvider>
-      <DataProvider>
-        <Wrapper>
-          <Header />
-          <Switch>
-            <Route exact path="/login" component={LoginForm} />
-            <Route exact path="/register" component={RegisterForm} />
-            {show && (
-              <Route
-                exact
-                path="/"
-                component={() => <KeySmashGame show={show} setShow={setShow} />}
-              />
-            )}
-            <Route exact path="/typing-game" component={() => <TypingGame />} />
-          </Switch>
-        </Wrapper>
-      </DataProvider>
-    </AuthProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
-
-const Wrapper = styled.div`
-  overflow-x: hidden;
-  width: 100%;
-  height: 100%;
-`;
